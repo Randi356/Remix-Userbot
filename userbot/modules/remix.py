5871f445bcd161f04d763f63a1f7e084e613309c
Randy Wahidin <63757267+Randi356@users.noreply.github.com>
2021-10-31 05:41:12 +0700

    changed

--- userbot/modules/remix.py
+++ userbot/modules/remix.py
@@ -1,4 +1,4 @@
-# import userbot by apis
+# import userbot by kontol
 
 from time import sleep
 from userbot import ALIVE_NAME, CMD_HELP
@@ -47,29 +47,29 @@ async def typewriter(typew):
 # Create by myself @localheart
 
 
-@register(outgoing=True, pattern="^.king(?: |$)(.*)")
+@register(outgoing=True, pattern="^.kontol(?: |$)(.*)")
 async def _(event):
     event.pattern_match.group(1)
     await event.edit(
-        f"`Halo {ALIVE_NAME} Saya Adalah Bot King Yang Menjaga Akun ini`"
+        f"`Halo {ALIVE_NAME} Saya Adalah Bot Remix Yang Menjaga Akun ini`"
         f"`Agar Terhindar Dari Orang Orang Jahat Di Telegram`"
         f"\n---------------------------------------------------"
-        f"\n__**(C) copyright 2021 King-Userbot USERBOT TELEGRAM**__"
-        f"\n\n**My King :** `{ALIVE_NAME}`")
+        f"\n__**(C) copyright 2021 Remix-Userbot USERBOT TELEGRAM**__"
+        f"\n\n**My Remix :** `{ALIVE_NAME}`")
 
 CMD_HELP.update(
     {
-        "king": "**✘ Plugin :** `King`\
-        \n\n  •  **Perintah :** `.king`\
-        \n  •  **Function : **Perkenalan Userbot King\
+        "king": "**✘ Plugin :** `Remix`\
+        \n\n  •  **Perintah :** `.kontol`\
+        \n  •  **Function : **Perkenalan Userbot Remix\
         \n\n  •  **Perintah :** `.sadboy`\
         \n  •  **Function : **Jadi sadboy:)\
         \n\n  •  **Perintah :** `.punten` | `.pantau`\
         \n  •  **Function : **Untuk punten dan pantau\
         \n\n  ** Perintah kosong **\
-        \n  ** Harap chat developer king @PacarFerdilla Jika ingin mengidekan sesuatu yang menarik **\
+        \n  ** Harap chat developer Remix @FlashProSpeed Jika ingin mengidekan sesuatu yang menarik **\
         \n\n  ** Perintah kosong **\
-        \n  ** Harap chat developer king @PacarFerdilla Jika ingin mengidekan sesuatu yang menarik **\
+        \n  ** Harap chat developer Remix @FlashProSpeed Jika ingin mengidekan sesuatu yang menarik **\
     "
     }
 )
