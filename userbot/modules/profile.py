dcaf92823bf1f5fe8cce51cf9ecd7ccfb47f8c89
Randy Wahidin <63757267+Randi356@users.noreply.github.com>
2021-11-02 01:45:25 +0700

    none

--- userbot/modules/profile.py
+++ userbot/modules/profile.py
@@ -21,16 +21,16 @@ from userbot import CMD_HELP, bot
 from userbot.events import register
 
 # ====================== CONSTANT ===============================
-INVALID_MEDIA = "```Maaf King Media Tidak Valid```"
-PP_CHANGED = "```King, Foto Profil Anda Telah Berhasil Diubah```"
-PP_TOO_SMOL = "```Gambar Terlalu Kecil King, Mohon Gunakan Yang Lebih Besar```"
+INVALID_MEDIA = "```Maaf Remix Media Tidak Valid```"
+PP_CHANGED = "```Remix, Foto Profil Anda Telah Berhasil Diubah```"
+PP_TOO_SMOL = "```Gambar Terlalu Kecil Remix, Mohon Gunakan Yang Lebih Besar```"
 PP_ERROR = "```Kegagalan Terjadi Saat Proses Gambar, Foto Profil Gagal Diubah```"
 
-BIO_SUCCESS = "```King, Bio Anda Telah Berhasil Diubah```"
+BIO_SUCCESS = "```Remix, Bio Anda Telah Berhasil Diubah```"
 
-NAME_OK = "```King, Nama Anda Telah Berhasil Diubah```"
+NAME_OK = "```Remix, Nama Anda Telah Berhasil Diubah```"
 USERNAME_SUCCESS = "```Username Anda Sudah Diubah```"
-USERNAME_TAKEN = "```Mohon Maaf King, Username Itu Sudah Ada Yang Menggunakannya```"
+USERNAME_TAKEN = "```Mohon Maaf Remix, Username Itu Sudah Ada Yang Menggunakannya```"
 # ===============================================================
 
 
